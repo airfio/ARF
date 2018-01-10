bf4693f19b687d3ca225588fd1869f21778d6c2d
Admin <33081124+airfio@users.noreply.github.com>
2018-01-10 20:03:02 +0530

    Upload 1
    N

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define ARFCOIN-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "ARFCOINURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
